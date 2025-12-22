b21542e897850d6538d147664ece92dec92eeb26
tariq <tariqmohd643@gmail.com>
2025-12-22 11:43:26 +0530

    graph hard problem

--- graphs/hardproblems/ShortestPathInUndirectedGraphWithUnitWeight.java
+++ graphs/hardproblems/ShortestPathInUndirectedGraphWithUnitWeight.java
@@ -1,9 +1,25 @@
 package dsa.striver.graphs.hardproblems;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ShortestPathInUndirectedGraphWithUnitWeight {
+
+    private void bfs (int src, List<List<Integer>> adj, int[] distance) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+
+        while (!queue.isEmpty()) {
+
+            int node = queue.poll();
+            for (int it : adj.get(node)) {
+                if (distance[node] + 1 < distance[it]) {
+                    distance[it] = distance[node] + 1;
+                    queue.add(node);
+                }
+            }
+        }
+    }
 
     public int[] shortestPath(int[][] edges, int N, int M) {
 
@@ -13,7 +29,26 @@ public class ShortestPathInUndirectedGraphWithUnitWeight {
             adj.add(new ArrayList<>());
         }
 
+        // add the edges of the graph
+        for (int i = 0; i < M; i++) {
+            int u = edges[i][0]; // source node
+            int v = edges[i][1]; // destination node
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+
         int[] distance = new int[N];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        bfs(0, adj, distance);
+
+        for (int i = 0; i < N; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                distance[i] = -1; // set the value to -1 if unreachable
+            }
+        }
+
         return distance;
     }
 }
